df96121f42282e03c323bf48363de1b8443041d7
n-prat <1504071+n-prat@users.noreply.github.com>
2023-05-05 15:39:16 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+#
+
+Small util allowing to print the Star Citizen joystick keybinds,
+directly updated by reading the exported `layout.xml`.
+
+## Setup
+
+- an editable `pdf` (ie a form)
+  - MUST have all the required text forms with the correct labels
+- an exported `xml` eg `layout_vkb_custom_v1_exported.xml` from Star Citizen containing the keybinds
+- example: if the keybind in `xml` is `js1_button5`, the `pdf` MUST have a label `button5`(or `js1_button5`)
---
+++ src/main.rs
@@ -0,0 +1,59 @@
+use clap::Parser;
+use std::io::Error;
+use std::path::PathBuf;
+use std::time::Instant;
+
+mod merge;
+
+/// https://github.com/J-F-Liu/lopdf/blob/master/examples/extract_toc.rs
+///
+#[derive(Parser, Debug)]
+#[clap(
+    author,
+    version,
+    about,
+    long_about = "Extract TOC and write to file.",
+    arg_required_else_help = true
+)]
+pub struct Args {
+    #[clap(num_args = 1..)]
+    pub pdf_paths: Vec<PathBuf>,
+
+    /// Optional output directory. If omitted the directory of the PDF file will be used.
+    #[clap(short, long)]
+    pub output: Option<PathBuf>,
+
+    /// Optional pretty print output.
+    #[clap(short, long)]
+    pub pretty: bool,
+}
+
+impl Args {
+    pub fn parse_args() -> Self {
+        Args::parse()
+    }
+}
+
+fn main() -> Result<(), Error> {
+    let args = Args::parse_args();
+
+    let start_time = Instant::now();
+    let pdf_paths = args
+        .pdf_paths
+        .iter()
+        .map(|pdf_path| {
+            PathBuf::from(
+                shellexpand::full(pdf_path.to_str().unwrap())
+                    .unwrap()
+                    .to_string(),
+            )
+        })
+        .collect();
+    println!("pdf_paths : {:?}", pdf_paths);
+
+    // TODO https://www.dariocancelliere.it/blog/2020/09/29/pdf-manipulation-with-rust-and-considerations
+    // "Filling form fields"
+
+    // TODO read multiple pdfs
+    merge::merge(pdf_paths)
+}
