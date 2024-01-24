b1fa00fa0d9e9df235b9a690f7425dca42b4ccb2
n-prat <1504071+n-prat@users.noreply.github.com>
2024-01-24 12:04:40 +0100

    vkb: improve description
    Add support for a user provided extended description (.csv)
    
    - vkb_button: list all button with user-friendly description
    - replace println by log

--- src/vkb/mod.rs
+++ src/vkb/mod.rs
@@ -28,7 +28,18 @@ pub(crate) fn parse_report(xml_path: PathBuf) -> Result<VkbBindings, VkbError> {
     Ok(VkbBindings { vkb_report })
 }
 
-pub(crate) fn check_report(vkb_binding: VkbBindings) -> vkb_button::ButtonMap {
-    let vkb_buttons = vkb_button::ButtonMap::try_from(vkb_binding.vkb_report).unwrap();
+pub(crate) fn check_report(
+    vkb_binding: VkbBindings,
+    vkb_user_provided_data: Option<csv::Reader<std::fs::File>>,
+) -> vkb_button::ButtonMap {
+    let mut vkb_buttons = vkb_button::ButtonMap::try_from(vkb_binding.vkb_report).unwrap();
+
+    match vkb_user_provided_data {
+        Some(vkb_user_provided_data) => {
+            vkb_buttons.inject_user_provided_desc(vkb_user_provided_data)
+        }
+        None => {}
+    }
+
     vkb_buttons
 }
