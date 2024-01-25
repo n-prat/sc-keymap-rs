7302b9197952dd2acb89e6a8dbea4c048630ab3d
n-prat <1504071+n-prat@users.noreply.github.com>
2024-01-25 12:46:24 +0100

    vkb: finally fix duplicate detection

--- src/button.rs
+++ src/button.rs
@@ -2,7 +2,7 @@
 //! They are usually obtained after parsing a joystick configuration directly; NOT from exported game mapping.
 //!
 
-#[derive(PartialEq, Debug, Clone)]
+#[derive(PartialEq, Clone)]
 pub(crate) enum TempoKind {
     /// Short+Long press
     /// "second line pulse length is equal to T_Tgl value in no matter to real depressing time"
@@ -21,7 +21,28 @@ pub(crate) enum TempoKind {
     },
 }
 
-#[derive(PartialEq, Debug, Clone)]
+impl core::fmt::Debug for TempoKind {
+    fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+        match self {
+            TempoKind::_Tempo1 => write!(f, "Tempo1[]"),
+            TempoKind::Tempo2 {
+                button_id_short,
+                button_id_long,
+            } => write!(f, "Tempo2[{}/{}]", button_id_short, button_id_long),
+            TempoKind::Tempo3 {
+                button_id_short,
+                button_id_long,
+                button_id_double,
+            } => write!(
+                f,
+                "Tempo3[{}/{}/{}]",
+                button_id_short, button_id_long, button_id_double
+            ),
+        }
+    }
+}
+
+#[derive(PartialEq, Clone)]
 pub(crate) enum ShiftKind {
     Shift1 {
         button_id_shift1: u8,
@@ -33,6 +54,21 @@ pub(crate) enum ShiftKind {
         button_id_shift1: u8,
         button_id_shift2: u8,
     },
+}
+
+impl core::fmt::Debug for ShiftKind {
+    fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+        match self {
+            ShiftKind::Shift1 { button_id_shift1 } => write!(f, "Shift1[{}]", button_id_shift1),
+            ShiftKind::Shift2 { button_id_shift2 } => {
+                write!(f, "Shift2[{}]", button_id_shift2)
+            }
+            ShiftKind::Shift12 {
+                button_id_shift1,
+                button_id_shift2,
+            } => write!(f, "Shift12[{}/{}]", button_id_shift1, button_id_shift2),
+        }
+    }
 }
 
 /// For now this is based on VKB buttons, but it should be applicable to all joysticks
@@ -62,7 +98,7 @@ pub(crate) enum PhysicalButtonKind {
 }
 
 /// This SHOULD(is) pretty generic; ie not really related to VKB
-#[derive(PartialEq, Debug, Clone)]
+#[derive(PartialEq, Clone)]
 pub(crate) enum ButtonKind {
     /// This matches a "b2" field in xml
     /// To get the ID we need to parse the desc...
@@ -79,15 +115,42 @@ pub(crate) enum ButtonKind {
     Virtual { id: u8 },
 }
 
+impl core::fmt::Debug for ButtonKind {
+    fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+        match self {
+            ButtonKind::Physical {
+                id,
+                kind,
+                info,
+                extended_desc,
+            } => write!(
+                f,
+                "PhysicalButton [{} {} {} ({kind:?})]",
+                id, info, extended_desc
+            ),
+            ButtonKind::Virtual { id } => {
+                write!(f, "VirtualButton [{}]", id)
+            }
+        }
+    }
+}
+
 /// This is NOT from the xml, this is the end result.
 /// We construct the final "buttons" by parsing the "xml_desc" field eg:
 ///
 /// <b>#6 </b> Joystick button : #52
 /// <b>#6 (F1) </b><b>TEMPO </b>\r\nVirtual button Short #6\r\nVirtual button Long #96
 /// etc
-#[derive(PartialEq, Debug, Clone)]
+#[derive(PartialEq, Clone)]
 pub(crate) struct Button {
     pub(crate) kind: ButtonKind,
+}
+
+impl core::fmt::Debug for Button {
+    fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+        // Skip self, and forward directly to `ButtonKind`
+        write!(f, "{:?}", self.kind)
+    }
 }
 
 impl Button {
